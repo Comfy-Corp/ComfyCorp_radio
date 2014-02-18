ea6d7fb66467422481c0d128233a67cb22bdfdcc
Guus Beckett <ghbeckett@gmail.com>
2014-02-18 10:48:33 +0100

    Interrupt based keypresses

--- ipac-base/include/ui.h
+++ ipac-base/include/ui.h
@@ -1,9 +1,8 @@
 #define UISTATE_SHOWTIME 0
-#define UISTATE_SHOWMENU 1
-#define UISTATE_SHOWSETUP 2
-#define UISTATE_SHOWRESET 3
-#define UISTATE_SHOWALARM 4
-#define UISTATE_SHOWSYNCING 5
+#define UISTATE_SHOWALARM 1
+#define UISTATE_SHOWSYNCING 2
+#define UISTATE_SHOWSETUP 3
+#define UISTATE_SHOWRESET 4
 
 typedef struct menuItem {
 	char* displayableText;
@@ -18,3 +17,6 @@ typedef struct menuItemContainer {
 
 int UIshow();
 void UIchangeState(char);
+int UIGetState();
+int UIScreenUp();
+int UIScreenDown();
--- ipac-base/source/ui.c
+++ ipac-base/source/ui.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "lcd.h"
 #include "ui.h"
+#include "userinput.h"
+#include "keyboard.h"
 
 char screenStateChar = UISTATE_SHOWTIME;
 
@@ -10,16 +12,21 @@ int UIshow()
 	switch (screenStateChar)
         {
             case UISTATE_SHOWTIME:
+            LcdWriteString("Ze Time",8);
                 break;
             case UISTATE_SHOWSYNCING:
+            LcdWriteString("SYNCING",8);
                 break;
             case UISTATE_SHOWALARM:
+            LcdWriteString("ALARMZZ",8);
                 break;
             case UISTATE_SHOWRESET:
+            LcdWriteString("RESET ME",9);
                 break;
             case UISTATE_SHOWSETUP:
+            LcdWriteString("SETUP SCREEN",13);
                 break;
-            case UISTATE_SHOWMENU:
+            default:
                 break;
         }
 	return screenStateChar;
@@ -29,6 +36,73 @@ void UIchangeState(char newState)
 {
 	screenStateChar = newState;
 	UIshow();
+}
+
+int UIGetState()
+{
+    return screenStateChar;
+}
+
+int UIScreenUp()
+{
+    ++screenStateChar;
+    if (screenStateChar > 2)
+    {
+        screenStateChar = 0;
+    }
+    UIshow();
+    printf("%s\n", "Iraan houdt zich niet aan de afspraken over nucliare uranium verrijking!");
+}
+
+int UIScreenDown()
+{
+    --screenStateChar;
+    if (screenStateChar < 0)
+    {
+        screenStateChar = 2;
+    }
+    UIshow();
+}
+
+int UIScreenOK()
+{
+    if (screenStateChar == UISTATE_SHOWSYNCING)
+    {
+        //TODO ADD ONLINE SETTINGS SYNCING METHODE
+        printf("%s\n","I would like to sync, but I can not do that yet :(" );
+    }
+}
+
+int UIScreenLeft()
+{
+    if (screenStateChar == UISTATE_SHOWALARM)
+    {
+        //TODO ADD SCROLLING ALARMS
+        printf("%s\n","I would like to go left, but I can not do that yet :(" );
+    }
+}
+
+int UIScreenRight()
+{
+    if (screenStateChar == UISTATE_SHOWALARM)
+    {
+        //TODO ADD SCROLLING ALARMS
+        printf("%s\n","I would like to go right, but I can not do that yet :(" );
+    }    
+}
+
+int UIScreenEsc()
+{
+    screenStateChar = 0;
+    UIshow();
+}
+
+int UIHandleInput(int kb_error)
+{
+        if (kb_error != KB_ERROR)
+        {
+            userInputKeyPress();
+        }
 }
 
 /*struct menuItem[] mainMenu = {
